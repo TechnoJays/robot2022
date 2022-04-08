9f56bda74689628a50f49da5b3f54b6453b0a765
Alex Mays <alex.mays@zf.com>
2022-04-08 18:34:38 -0400

    bug fix and disable 2 camera mode

--- src/subsystems/vision.py
+++ src/subsystems/vision.py
@@ -1,10 +1,14 @@
 from cscore import CameraServer
 
 def main():
-    cs = CameraServer.getInstance()
-    cs.enableLogging()
+    cs1 = CameraServer.getInstance()
+    cs1.enableLogging()
 
-    usb1 = cs.startAutomaticCapture(dev=0)
-    usb2 = cs.startAutomaticCapture(dev=1)
+    cs2 = CameraServer.getInstance()
+    cs2.enableLogging()
 
-    cs.waitForever()
+    usb1 = cs1.startAutomaticCapture(dev=0)
+    usb2 = cs2.startAutomaticCapture(dev=1)
+
+    cs1.waitForever()
+    cs2.waitForever()
